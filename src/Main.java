462f080131737b2be09c37fabd7ed20ddc3005ac
Yun Sofia <sonyayun@yandex.ru>
2022-01-31 14:59:39 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int initialBalance = 100;
+        int refill = 1100;
+        int bonus = refill / 100;
+
+        if (refill > 1000) {
+        } else {
+            bonus = 0;
+        }
+        int endBalance = initialBalance + refill + bonus;
+        System.out.println(endBalance + " рублей Ваш итоговый счет.");
+        System.out.println(bonus + " бонусных рублей в том числе.");
+        }
+}
